98de091cd3a92b33e78aff237e8f3797afd55c86
PhidelCastra <58807845+PhidelCastra@users.noreply.github.com>
2020-06-15 12:07:45 +0200

    Add some comments

--- TreeHunter/WoodButcher.Request/RequestHandler.cs
+++ TreeHunter/WoodButcher.Request/RequestHandler.cs
@@ -9,25 +9,32 @@ namespace WoodButcher.Request
 {
     public class RequestHandler<T>
     {
+        /// <summary>
+        /// URL for request data.
+        /// </summary>
         private string _url = "https://www.berlin.de/ba-steglitz-zehlendorf/politik-und-verwaltung/aemter/strassen-und-gruenflaechenamt/gruenflaechen/baeume/baumfaellungen/index.php/index/all.json?q=";
 
         /* Contructor */
         public RequestHandler() { }
 
+        /// <summary>
+        /// Gets a adata set from the URL and prepare a List of corresponding objects.
+        /// </summary>
+        /// <returns>List with objects of generic model T.</returns>
         public List<T> GetRequestResult()
         {
-            // Grab data from the tree json.
+            // Grab data.
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(_url);
             request.ContentType = "json/text";
             request.Method = "POST";
 
             var response = (HttpWebResponse)request.GetResponse();
             var reader = new StreamReader(response.GetResponseStream());
-            var r = reader.ReadToEnd();
-            var json = JObject.Parse(r);
-            var token = json["index"].ToString();
+            var stringResult = reader.ReadToEnd();
+            var jsonResult = JObject.Parse(stringResult);
+            var readFrom = jsonResult["index"].ToString();
 
-            var falledTrees = JsonConvert.DeserializeObject<List<T>>(token);
+            var falledTrees = JsonConvert.DeserializeObject<List<T>>(readFrom);
             return falledTrees;
         }
     }
